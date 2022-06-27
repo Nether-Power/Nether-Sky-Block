bf9de3c65283d63fefbf1204615d3a9f74b03485
DancingSnow <1121149616@qq.com>
2022-06-27 16:12:11 +0800

    添加灵魂沙峡谷生成女巫

--- src/main/java/dev/dubhe/skyland/SkyLand.java
+++ src/main/java/dev/dubhe/skyland/SkyLand.java
@@ -1,6 +1,9 @@
 package dev.dubhe.skyland;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -15,7 +18,13 @@ public class SkyLand implements ModInitializer {
 
     public static final RegistryKey<WorldPreset> SKYLAND = RegistryKey.of(Registry.WORLD_PRESET_KEY, ID);
 
+    private static final Identifier SOUL_SAND_VALLEY = new Identifier("soul_sand_valley");
+
     @Override
     public void onInitialize() {
+
+        BiomeModifications.addSpawn(context -> SOUL_SAND_VALLEY.equals(context.getBiomeKey().getValue()), SpawnGroup.MONSTER,
+                EntityType.WITCH, 1, 1, 1);
+
     }
 }
