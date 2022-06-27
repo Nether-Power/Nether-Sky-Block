e2121e3ac1a2b791c468aad0df5d7db3f94fce1e
DancingSnow <1121149616@qq.com>
2022-06-28 01:35:05 +0800

    Update SkyLand

--- src/main/java/dev/dubhe/skyland/SkyLand.java
+++ src/main/java/dev/dubhe/skyland/SkyLand.java
@@ -2,21 +2,24 @@ package dev.dubhe.skyland;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.GameRules;
+import net.minecraft.world.GameRules.BooleanRule;
+import net.minecraft.world.GameRules.Category;
+import net.minecraft.world.GameRules.Key;
 import net.minecraft.world.gen.WorldPreset;
 
 public class SkyLand implements ModInitializer {
-    public static final Identifier ID = new Identifier("skyland", "skyland");
 
-    public static final GameRules.Key<GameRules.BooleanRule> LC = GameRules.register("qnmdLC",
-            GameRules.Category.MOBS, GameRules.BooleanRule.create(false));
-    public static final GameRules.Key<GameRules.BooleanRule> CHIEFTAIN = GameRules.register("chieftainMode",
-            GameRules.Category.MISC, GameRules.BooleanRule.create(false));
+    public static final String MOD_ID = "skyland";
+    public static final Identifier ID = new Identifier(SkyLand.MOD_ID, "skyland");
+
+    public static final Key<BooleanRule> LC = GameRuleRegistry.register("gnmdLC", Category.MOBS, BooleanRule.create(false));
+    public static final Key<BooleanRule> CHIEFTAIN = GameRuleRegistry.register("chieftainMode", Category.MISC, BooleanRule.create(false));
 
     public static final RegistryKey<WorldPreset> SKYLAND = RegistryKey.of(Registry.WORLD_PRESET_KEY, ID);
 
