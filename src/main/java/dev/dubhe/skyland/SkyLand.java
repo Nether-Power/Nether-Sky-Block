34a85de9254d4af338af14d13b22af7f83d3e58b
Gu_ZT <administration@ztxy666.cn>
2022-06-28 01:09:09 +0800

    Update SkyLand.java

--- src/main/java/dev/dubhe/skyland/SkyLand.java
+++ src/main/java/dev/dubhe/skyland/SkyLand.java
@@ -13,8 +13,10 @@ import net.minecraft.world.gen.WorldPreset;
 public class SkyLand implements ModInitializer {
     public static final Identifier ID = new Identifier("skyland", "skyland");
 
-    public static final GameRules.Key<GameRules.BooleanRule> LC = GameRules.register("qnmdLC", GameRules.Category.MOBS, GameRules.BooleanRule.create(false));
-    public static final GameRules.Key<GameRules.BooleanRule> CHIEFTAIN = GameRules.register("chieftainMode", GameRules.Category.MISC, GameRules.BooleanRule.create(false));
+    public static final GameRules.Key<GameRules.BooleanRule> LC = GameRules.register("qnmdLC",
+            GameRules.Category.MOBS, GameRules.BooleanRule.create(false));
+    public static final GameRules.Key<GameRules.BooleanRule> CHIEFTAIN = GameRules.register("chieftainMode",
+            GameRules.Category.MISC, GameRules.BooleanRule.create(false));
 
     public static final RegistryKey<WorldPreset> SKYLAND = RegistryKey.of(Registry.WORLD_PRESET_KEY, ID);
 
