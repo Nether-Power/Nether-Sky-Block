16f0d8b1dc2d77c5b6dc6d9e41b482351a0fd352
DancingSnow <1121149616@qq.com>
2022-06-29 22:17:42 +0800

    remove TODO

--- src/main/java/dev/dubhe/skyland/mixin/WanderingTraderManagerMixin.java
+++ src/main/java/dev/dubhe/skyland/mixin/WanderingTraderManagerMixin.java
@@ -9,8 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(WanderingTraderManager.class)
-//TODO 使下界群系可以生成骑着赤足兽的游商（玩家40格半径最高点为熔岩时，每20min有2.5%概率生成，当第一次生成失败时后续生成概率提高至7.5%）
-//TODO 游商额外出售物品
+//TODO 使下界群系可以生成骑着赤足兽的游商（玩家40格半径最高点为熔岩时，每20min有2.5%概率生成，当第一次生成失败时后续生成概率提高至7.5%
 public class WanderingTraderManagerMixin {
 
     @Redirect(method = "trySpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I"))
