e4a2754015b3b1eeff71db4d0e8940ffc6eba974
DancingSnow <1121149616@qq.com>
2022-06-30 17:54:33 +0800

    添加一个命令，一次性开启 SkyLand 规则

--- src/main/java/dev/dubhe/skyland/SkyLandMod.java
+++ src/main/java/dev/dubhe/skyland/SkyLandMod.java
@@ -1,6 +1,10 @@
 package dev.dubhe.skyland;
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
@@ -8,6 +12,9 @@ import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
@@ -45,6 +52,27 @@ public class SkyLandMod implements ModInitializer {
                 world.spawnEntityAndPassengers(itemEntity);
             }
         });
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(CommandManager.literal("skyland")
+                .then(CommandManager.literal("gamerule")
+                        .then(CommandManager.argument("boolean", BoolArgumentType.bool())
+                                .executes(context -> {
+                                    ServerCommandSource serverCommandSource = context.getSource();
+                                    boolean bool = BoolArgumentType.getBool(context, "boolean");
+                                    if (bool) {
+                                        serverCommandSource.getServer().getGameRules().get(SkyLandGamerules.WATER_CAULDRON).set(true, serverCommandSource.getServer());
+                                        serverCommandSource.getServer().getGameRules().get(SkyLandGamerules.ICE_GOLEM).set(true, serverCommandSource.getServer());
+                                        serverCommandSource.getServer().getGameRules().get(SkyLandGamerules.NETHER_TRADER).set(true, serverCommandSource.getServer());
+                                        serverCommandSource.sendFeedback(Text.translatable("skyland.command.gamerule_set_succeed", "true"), true);
+                                    } else {
+                                        serverCommandSource.getServer().getGameRules().get(SkyLandGamerules.WATER_CAULDRON).set(false, serverCommandSource.getServer());
+                                        serverCommandSource.getServer().getGameRules().get(SkyLandGamerules.ICE_GOLEM).set(false, serverCommandSource.getServer());
+                                        serverCommandSource.getServer().getGameRules().get(SkyLandGamerules.NETHER_TRADER).set(false, serverCommandSource.getServer());
+                                        serverCommandSource.sendFeedback(Text.translatable("skyland.command.gamerule_set_succeed", "false"), true);
+                                    }
+                                    return 1;
+                                }))
+                )));
 
     }
 }
