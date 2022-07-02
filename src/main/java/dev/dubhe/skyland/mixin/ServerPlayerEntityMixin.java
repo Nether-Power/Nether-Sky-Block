0ee8329898a503cc082388154d6f9a1e448c6417
DancingSnow <60736156+DancingSnow0517@users.noreply.github.com>
2022-07-02 16:45:37 +0800

    Update ServerPlayerEntityMixin.java

--- src/main/java/dev/dubhe/skyland/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/dubhe/skyland/mixin/ServerPlayerEntityMixin.java
@@ -1,10 +1,16 @@
 package dev.dubhe.skyland.mixin;
 
+import dev.dubhe.skyland.SkyLandGamerules;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ComposterBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.WorldSavePath;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
